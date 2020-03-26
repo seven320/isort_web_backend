6654ea8d1d0ba33b3bbc4dee1d07ccc11efba09c
seven320 <yosyuaomenw@yahoo.co.jp>
2020-03-26 23:39:38 +0900

    add test code and post

--- test/sample_app.py
+++ test/sample_app.py
@@ -5,19 +5,28 @@ import falcon
 
 class AppResource(object):
     def on_get(self, req, resp):
+        """Handles GET requests """
         msg = {
-            "message":"falcon is fine!!"
+            "message":"hello falcon"
         }
+        resp.status = falcon.HTTP_200
         resp.body = json.dumps(msg)
 
-app = falcon.API()
-app.add_route("/", AppResource())
+    def on_post(self, req, res):
+        msg = {
+            "message":"import numpy as np\n\nimport hoge"
+        }
+        res.body = json.dumps(msg)
+
+def create_app():
+    app = falcon.API()
+    app.add_route("/", AppResource())
+
+    return app
 
 if __name__ == "__main__":
     from wsgiref import simple_server
 
+    app = create_app()
     httpd = simple_server.make_server("127.0.0.1", 3000, app)
     httpd.serve_forever()
-
-
-    
